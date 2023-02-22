376276a22e16d156ae9879feb1f1fef83fc2e88a
Arik Maor <arikmaor@gmail.com>
2023-02-22 14:12:43 +0200

    Demonstrate tar-stream compilation failure

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import tar from 'tar-stream';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,3 +18,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+const pack = tar.pack();
+pack.on('data', (data) => console.log(data));
+pack.entry({name: 'test.txt'}, 'test content');
+pack.finalize();
